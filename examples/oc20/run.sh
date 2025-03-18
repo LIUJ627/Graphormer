7d96201e28eaef86b50c0570c1c0d71b81693693
Jinliang_Liu <1067122749@qq.com>
2025-03-18 16:52:52 +0800

    modify hyperparameter

---
+++ examples/oc20/run.sh
@@ -0,0 +1,12 @@
+fairseq-train --user-dir ~/Graphormer/fairseq/examples/graphormer  \
+   ./data_noedge --valid-subset val_id --best-checkpoint-metric loss \
+   --num-workers 0 --ddp-backend=c10d \
+   --task is2re --criterion mae_deltapos --arch graphormer3d_base  \
+   --optimizer adam --adam-betas '(0.9, 0.98)' --adam-eps 1e-6 --clip-norm $clip_norm 5 \
+   --lr-scheduler polynomial_decay --lr 3e-4 --warmup-updates 10  --total-num-update 100 --batch-size 1 \
+   --dropout 0.0 --attention-dropout 0.1 --weight-decay 0.001 --update-freq 1 --seed 1 \
+   --fp16 --fp16-init-scale 4 --fp16-scale-window 128 --tensorboard-logdir ./tsbs \
+   --embed-dim 384 --ffn-embed-dim 384 --attention-heads 24 \
+   --max-update 100 --log-interval 10 --log-format simple \
+   --save-interval-updates 50 --validate-interval-updates 25 --keep-interval-updates 30 --no-epoch-checkpoints  \
+   --save-dir ./ckpt --layers 12 --blocks 4 --required-batch-size-multiple 1  --node-loss-weight 15 --num-kernel 2
